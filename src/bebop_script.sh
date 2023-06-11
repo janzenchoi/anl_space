7b48969af3bd74e8b7d349444b00b84e664567d7
janzen <janzenchoi@yahoo.com>
2023-06-11 14:58:50 -0500

    added simulation

---
+++ src/bebop_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --account=STARTUP-CHOIH
+#SBATCH --partition=bdwall
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=01:00:00
+
+NUM_PROCESSORS=8
+DEER_PATH="$HOME/src/deer"
+SIMULATION_FILE="simulation.i
+srun -np $NUM_PROCESSORS $DEER_PATH -i $SIMULATION_FILE
