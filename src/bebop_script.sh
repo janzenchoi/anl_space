b6ea6cb0f71602af23fb1f2f2122c565225f1f6b
janzen <janzenchoi@yahoo.com>
2023-06-11 15:30:52 -0500

    changed deer path

--- src/bebop_script.sh
+++ src/bebop_script.sh
@@ -7,6 +7,6 @@
 #SBATCH --time=01:00:00
 
 NUM_PROCESSORS=8
-DEER_PATH="$HOME/src/deer"
+DEER_PATH="$HOME/src/deer/deer-opt"
 SIMULATION_FILE="simulation.i"
 srun -n $NUM_PROCESSORS $DEER_PATH -i $SIMULATION_FILE
